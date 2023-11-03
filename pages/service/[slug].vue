d4ae0dc911a7aecccd8be72c2c97af1033cdb737
Thaetami <tatanghaetami.97@gmail.com>
2023-11-03 09:29:41 +0700

    update service api [slug] and service page

--- pages/service/[slug].vue
+++ pages/service/[slug].vue
@@ -11,10 +11,28 @@
         </div>
         <div class="h-full w-full md:w-[65%] lg:w-[70%] md:border-l-2 md:border-green-200 px-3 md:px-4 md:pl-4 mt-3">
           <template v-if="pending">
-            <LoadingComp />
+            <div role="status" class="w-full kontener animate-pulse">
+              <div class="flex justify-center">
+                <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4" />
+              </div>
+              <div class="flex justify-center">
+                <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-20 mb-4" />
+              </div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-2.5" />
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-2.5" />
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 w-1/3 mb-2.5" />
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5" />
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-2.5" />
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 w-1/2 mb-2.5" />
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-2.5" />
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-2.5" />
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5 1/4" />
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 w-1/4 mb-2.5" />
+              <span class="sr-only">Loading...</span>
+            </div>
           </template>
           <template v-else>
-            <template v-if="service">
+            <template v-if="service.id">
               <div class="text-3xl font-bold text-center">
                 {{ service.title }}
               </div>
--- server/api/service/[slug].js
+++ server/api/service/[slug].js
@@ -4,7 +4,7 @@ export default defineEventHandler((event) => {
   const { slug } = event.context.params
 
   const allServices = getData()
-  const service = allServices.find(service => service.slug === slug) || null
+  const service = allServices.find(service => service.slug === slug) || {}
 
   return service
 })
